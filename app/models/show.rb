9f66dbe495c3828668d0f08382652fc6fa81f77f
github username <github email address>
2021-02-16 19:25:54 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+    
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map {|a| a.full_name}
+    end
+
+    def build_network(call_letters: call_letters)
+        network = Network.find_or_create_by(call_letters: call_letters)
+        network.shows << self
+    end
   
 end
---
+++ db/migrate/008_add_genres_to_show.rb
@@ -0,0 +1,6 @@
+class AddGenresToShow < ActiveRecord::Migration[5.1]
+    def change
+        add_column :shows, :genre, :string
+    end
+end
+    
